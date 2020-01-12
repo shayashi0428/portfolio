417b9c213478fbd1f181598bf366ae94dafce888
Sachie Hayashi <shayashi0428@gmail.com>
2020-01-12 01:00:56 -0800

    Create base files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$(function() {
+
+  /* ---------------- Navigation toggle button ---------------- */
+
+  $('#nav-toggle').click(function() {
+    $('#header-nav').toggleClass('nav-active');
+  });
+
+
+});
